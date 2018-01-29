2885f1b8957f9876304d0d8eb778ac57cb7eba8b
Michael9122 <mixail9122@gmail.com>
2018-01-29 23:59:11 +0300

    1.3. JavaDoc. Code convention[#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j;
 
-class Calculate {
+/**
+* Class Класс для вывода надписи "Hello World".
+* @author Michael Yakimenko
+* @since 29.01.2018
+* @version 1
+*/
 
+class Calculate {
+	
+	/**
+	* Метод для вывода "Hello World".
+	*/
 	public static void main (String[] args) {
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Michael Yakimenko (Mixail912@gmail.com)
+* @version 1
+* @since 29.01.2018
+*/
+package ru.job4j;
