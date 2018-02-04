343271c52018fe6130a976daffbbb2750547ed3c
Michael9122 <mixail9122@gmail.com>
2018-02-04 20:24:02 +0300

    Исправил класс Max. Удалил присваиваемые параметры.

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -3,8 +3,6 @@ package ru.job4j.max;
 public class Max {
 
     public static int max(int first, int second) {
-        first = 1;
-        second = 2;
         return first > second ? first : second;
     }
 }
