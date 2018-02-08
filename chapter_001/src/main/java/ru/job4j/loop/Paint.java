5eee466c787ebb03afc52b56782e1f28f574257b
Michael9122 <mixail9122@gmail.com>
2018-02-08 20:28:37 +0300

    4.4. Построить пирамиду в псевдографике. [#4412]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,70 @@
+package ru.job4j.loop;
+
+/**
+ * Класс для создания пирамиды в псевдографике.
+ * @author Michael Yakimenko (Mixail912@gmail.com)
+ * @since 08.02.2018
+ * @version 1
+ */
+public class Paint {
+
+    /**
+     * Method rightTrl.
+     * @param height это высота пирамиды.
+     */
+    public String rightTrl(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
+    /**
+     * Method leftTrl.
+     * @param height это высота пирамиды.
+     */
+    public String leftTrl(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= weight - column - 1) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
+    /**
+     * Method pyramid.
+     * @param height это высота пирамиды.
+     */
+    public String pyramid(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = 2 * height - 1;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= height - column - 1 && row + height - 1 >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+}
