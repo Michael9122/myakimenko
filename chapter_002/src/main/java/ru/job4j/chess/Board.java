11e38788e101c33ba38edab53623744a68490f07
Michael9122 <mixail9122@gmail.com>
2018-03-26 20:34:14 +0300

    Каркас шахматной доски. Отредактировал код.

--- chapter_002/src/main/java/ru/job4j/chess/Board.java
+++ chapter_002/src/main/java/ru/job4j/chess/Board.java
@@ -20,6 +20,55 @@ public class Board {
     }
 
     /**
+     * Метод проверяет что в заданной ячейке есть фигруа.
+     * @param source
+     * @return
+     */
+    public Figure findFigure(Cell source) {
+        Figure result = null;
+        for (int i = 0; i < figures.length; i++) {
+            if (figures[i] != null && source.equals(figures[i].position)) {
+                result = figures[i];
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Метод проверяет что полученный путь не зданят фигурами.
+     * @param source
+     * @param dest
+     * @return
+     */
+    public boolean CheckWay(Cell source, Cell dest) {
+        Figure figure = findFigure(source);
+        boolean result = false;
+        Cell[] check = figure.way(source, dest);
+        for (int i = 0; i < check.length; i++) {
+            if (findFigure(check[i]) == null) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Метод проверяет что фигруа может так ходить.
+     * @param source
+     * @param dest
+     * @return
+     */
+    public boolean figureWay(Cell source, Cell dest) {
+        boolean result = false;
+        Figure figure = findFigure(source);
+            if (figure.way(source, dest) != null) {
+                result = true;
+            }
+        return result;
+    }
+
+    /**
      * Метод должен проверить
      * - Что в заданной ячейки есть фигура. если нет. то выкинуть исключение
      * - Если фигура есть. Проверить может ли она так двигаться. Если нет то упадет исключение
@@ -27,43 +76,23 @@ public class Board {
      * - Если все отлично. Записать в ячейку новое новое положение Figure figure.copy(Cell dest)
      * @param source
      * @param dest
-     * @return
+     * @return true.
      * @throws ImpossibleMoveException
      * @throws OccupiedWayException
      * @throws FigureNotFoundException
      */
     public boolean move(Cell source, Cell dest) throws ImpossibleMoveException, OccupiedWayException, FigureNotFoundException {
-        boolean result = false;
-        boolean findFigure = false;
-        int index = 0;
-
-        for (int i = 0; i < figures.length - 1; i++) {
-            if (figures[i] != null && source.equals(figures[i].position)) {
-                index = i;
-                findFigure = true;
-                break;
-            }
-        }
-        if (findFigure) {
-            if (figures[index].way(source, dest) != null) {
-                for (Cell way : figures[index].way(source, dest)) {
-                    for (int i = 0; i <= figures.length; i++) {
-                        if (figures[i] != null && (way.getX() == figures[i].position.getX()) && (way.getY() == figures[i].position.getY())) {
-                            throw new OccupiedWayException("Данная клетка уже занята.");
-                        } else {
-                            figures[index] = figures[index].copy(dest);
-                            result = true;
-                            break;
-                        }
-                    }
-                }
-            } else {
-                throw new ImpossibleMoveException("Фигура не может так ходить.");
-            }
-        } else {
+        Figure figure = findFigure(source);
+        if (this.findFigure(source) == null) {
             throw new FigureNotFoundException("Фигура не найдена.");
         }
-
-        return result;
+        if (!this.CheckWay(source, dest)) {
+            throw new OccupiedWayException("На пути есть другие фигуры.");
+        }
+        if (!this.figureWay(source, dest)) {
+            throw new ImpossibleMoveException("Фигура не может так ходить.");
+        }
+        figure = figure.copy(dest);
+        return true;
     }
 }
