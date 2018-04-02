9dc06ab1c5b91415a8e06f0ac7b925895929b422
Michael9122 <mixail9122@gmail.com>
2018-04-02 23:40:19 +0300

    Исправление ошибок.

--- chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
@@ -14,20 +14,23 @@ public class MenuTracker {
     private Tracker tracker;
     private List<UserAction> actions = new ArrayList<>();
 
-
     public MenuTracker(Input input, Tracker tracker) {
         this.input = input;
         this.tracker = tracker;
     }
 
     public void fillActions() {
-        this.actions.add(0, new AddItem(0,  "Add new item."));
-        this.actions.add(1, new ShowAll(1, "Show all items."));
-        this.actions.add(2, new EditItem(2, "Edit item."));
-        this.actions.add(3, new DeleteItem(3, "Delete item."));
-        this.actions.add(4, new FindItemById(4, "Find item by Id."));
-        this.actions.add(5, new FindItemsByName(5, "Find items by name."));
-        this.actions.add(6, new ExitProgram(6, "Exit Program."));
+        this.actions.add(new AddItem(0,  "Add new item."));
+        this.actions.add(new ShowAll(1, "Show all items."));
+        this.actions.add(new EditItem(2, "Edit item."));
+        this.actions.add(new DeleteItem(3, "Delete item."));
+        this.actions.add(new FindItemById(4, "Find item by Id."));
+        this.actions.add(new FindItemsByName(5, "Find items by name."));
+        this.actions.add(new ExitProgram(6, "Exit Program."));
+    }
+
+    public int getRange() {
+        return actions.size();
     }
 
     public void select(int key) {
@@ -113,7 +116,8 @@ public class MenuTracker {
 
         public void execute(Input input, Tracker tracker) {
             String id = input.ask("Please, enter the task's id: ");
-            tracker.findById(id);
+            Item item = tracker.findById(id);
+            System.out.println(String.format("%s. %s", item.getId(), item.getName()));
         }
     }
 
@@ -127,7 +131,9 @@ public class MenuTracker {
 
         public void execute(Input input, Tracker tracker) {
             String name = input.ask("Please, enter the task's name: ");
-            tracker.findByName(name);
+            for (Item item1 : tracker.findByName(name)) {
+                System.out.println(String.format("%s. %s", item1.getId(), item1.getName()));
+            }
         }
     }
 
