d2c8f98683a7f460da2c3ce4e5511c7f6fc2b339
Michael9122 <mixail9122@gmail.com>
2018-06-12 20:17:34 +0300

    Создать контейнер на базе связанного списка

---
+++ chapter_004/src/main/java/ru/job4j/list/ContainerLinkedList.java
@@ -0,0 +1,116 @@
+package ru.job4j.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @author Michael Yakimenko (Mixail912@gmail.com)
+ * @since 12.06.2018
+ * @version 1
+ */
+public class ContainerLinkedList<E> implements Iterable<E> {
+
+    /**
+     * Размер списка.
+     */
+    private int size = 0;
+
+    /**
+     * Указатель на первый элемент.
+     */
+    private Node<E> first;
+
+    /**
+     * Указатель на последний элемент.
+     */
+    private Node<E> last;
+
+    private int modCount = 0;
+
+    private int expectedModCount;
+
+    /**
+     * Конструктор класса.
+     */
+    public ContainerLinkedList() {
+    }
+
+    /**
+     * Добавление элемента в списаок.
+     * @param item элемент которуй нужно добавить.
+     */
+    public void add(E item){
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, item, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    /**
+     * Получение элемента по индексу.
+     * @param index индекс элемента.
+     * @return возвращает элемент.
+     */
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+
+        return new Iterator<E>() {
+
+            int count = 0;
+
+            E date;
+
+            public boolean hasNext() {
+                return (count < size) && (last != null);
+            }
+
+            public E next() {
+                checkForComodification();
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                count++;
+                date = first.item;
+                first = first.next;
+                return date;
+            }
+        };
+    }
+
+    /**
+     * Проверка на то, что с момента создания итератора коллекция не подверглась структурному изменению.
+     */
+    private final void checkForComodification() {
+        expectedModCount = modCount;
+        if (modCount != expectedModCount) {
+            throw new ConcurrentModificationException();
+        }
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/list/ContainerLinkedListTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Michael Yakimenko (Mixail912@gmail.com)
+ * @since 12.06.2018
+ * @version 1
+ */
+public class ContainerLinkedListTest {
+
+    private ContainerLinkedList<Integer> list = new ContainerLinkedList<>();
+
+    private Iterator<Integer> it = list.iterator();
+
+    @Before
+    public void setUp() throws Exception {
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+    }
+
+    @Test
+    public void whenGetElementThenGetThisElement() {
+        assertThat(list.get(0), is(1));
+        assertThat(list.get(1), is(2));
+        assertThat(list.get(2), is(3));
+        assertThat(list.get(3), is(4));
+    }
+
+    @Test
+    public void shouldReturn() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test
+    public void whenInvokeHasNextAndNextThenCheckTheGetResultsIsGoingToBeFalse() {
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(false));
+    }
+}
