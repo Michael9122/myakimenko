6a0906b68f46b9542777c8cd323b209109fd29c8
Michail Yakimenko <mailto:mixail912@gmail.com>
2018-07-08 01:25:19 +0300

    Set реализованный на связном списке.

--- chapter_004/src/main/java/ru/job4j/set/SimpleSetLinked.java
+++ chapter_004/src/main/java/ru/job4j/set/SimpleSetLinked.java
@@ -4,10 +4,20 @@ import ru.job4j.list.ContainerLinkedList;
 
 import java.util.Iterator;
 
+/**
+ * @author Michael Yakimenko (Mixail912@gmail.com)
+ * @since 08.07.2018
+ * @version 1
+ */
 public class SimpleSetLinked<E> implements Iterable<E> {
 
     ContainerLinkedList list = new ContainerLinkedList();
 
+    /**
+     * Метод проверяет совпадение элементов.
+     * @param e элемент.
+     * @return true если такой элемент уже есть, либо false если нет.
+     */
     public boolean contains(E e) {
         boolean result = false;
         for (Object element : list) {
--- chapter_004/src/test/java/ru/job4j/set/SimpleSetLinkedTest.java
+++ chapter_004/src/test/java/ru/job4j/set/SimpleSetLinkedTest.java
@@ -3,9 +3,16 @@ package ru.job4j.set;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Iterator;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
+/**
+ * @author Michael Yakimenko (Mixail912@gmail.com)
+ * @since 08.07.2018
+ * @version 1
+ */
 public class SimpleSetLinkedTest {
 
     SimpleSetLinked<Integer> list = new SimpleSetLinked<>();
@@ -16,11 +23,31 @@ public class SimpleSetLinkedTest {
         list.add(2);
         list.add(3);
         list.add(4);
+        list.add(5);
+        list.add(6);
     }
 
     @Test
     public void whenAddSameElementThenContainsTrue() {
         list.add(1);
         assertThat(list.contains(1), is(true));
+    }
+
+    @Test
+    public void iteratorTest() {
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(5));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(false));
     }
 }
