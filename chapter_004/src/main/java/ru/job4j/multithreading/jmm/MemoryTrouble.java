38a50c2fa689ded7abaf9f6dbec01605a8aa8036
Michail Yakimenko <mailto:mixail912@gmail.com>
2018-12-17 23:06:18 +0300

    Проиллюстрировать проблемы с многопоточностью.

---
+++ chapter_004/src/main/java/ru/job4j/multithreading/jmm/MemoryTrouble.java
@@ -0,0 +1,54 @@
+package ru.job4j.multithreading.jmm;
+
+/**
+ * @author Michael Yakimenko (Mixail912@gmail.com)
+ * @since 17.12.2018
+ * @version 1
+ */
+public class MemoryTrouble {
+
+    public static void main(String[] args) throws Exception {
+        Counter counter = new Counter();
+        counter.counter = 10;
+        MyThread myThread = new MyThread();
+        myThread.setName("thread one");
+        MyThread myThread2 = new MyThread();
+        myThread.setCounter(counter);
+        myThread2.setCounter(counter);
+        myThread.start();
+        myThread2.start();
+        myThread.join();
+        myThread2.join();
+        System.out.println(counter.counter);
+    }
+}
+
+class MyThread extends Thread {
+
+    Counter counter;
+
+    public void setCounter(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        counter.change();
+    }
+}
+
+class Counter {
+
+    int counter;
+
+    public void change() {
+        int counter = this.counter;
+        for (int i = 0; i < 10; i++) {
+            if (Thread.currentThread().getName().equals("thread one")) {
+                Thread.yield();
+            }
+            counter++;
+        }
+        this.counter = counter;
+    }
+}
