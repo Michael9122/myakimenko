a8a487d200e672a8e69ad2844cd9b8eef5ccd3b7
Michail Yakimenko <mailto:mixail912@gmail.com>
2019-02-20 20:17:47 +0300

    Удаление запрещенных слов

--- chapter_006/src/main/java/ru/job4j/inputoutput/CheckAbuses.java
+++ chapter_006/src/main/java/ru/job4j/inputoutput/CheckAbuses.java
@@ -1,11 +1,26 @@
 package ru.job4j.inputoutput;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 
+/**
+ * @author Michael Yakimenko (Mixail912@gmail.com)
+ * @since 20.02.2019
+ * @version 1
+ */
 public class CheckAbuses {
 
     public void dropAbuses(InputStream in, OutputStream out, String[] abuse) {
-
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                for (String s : abuse) {
+                    line = line.replaceAll(s, "");
+                }
+                writer.write(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
