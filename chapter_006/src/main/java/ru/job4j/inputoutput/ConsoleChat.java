a4a268f9cb2bd6c84a182d5b7b591eb761fd1805
Mikhail Yakimenko <Mixail9122@gmail.com>
2019-06-06 15:00:08 +0300

    Исправил ошибки checkStyle

--- chapter_006/src/main/java/ru/job4j/inputoutput/ConsoleChat.java
+++ chapter_006/src/main/java/ru/job4j/inputoutput/ConsoleChat.java
@@ -26,11 +26,11 @@ public class ConsoleChat {
         String result = null;
         Random rand = new Random();
         int n = 0;
-        for(Scanner sc = new Scanner(f); sc.hasNext(); ) {
-            ++n;
+        for (Scanner sc = new Scanner(f); sc.hasNext(); ++n) {
             String line = sc.nextLine();
-            if(rand.nextInt(n) == 0)
+            if (rand.nextInt(n) == 0) {
                 result = line;
+            }
         }
         return result;
     }
